8c6ba5581795cf4aa434f43aadca1c5e5e66b1d5
Tanner <Tanner.Hall@unb.ca>
2026-05-13 15:20:54 -0300

    code for question 1 for lab 2

---
+++ lab2/question1/intequiv.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("%s","character \t");
+    printf("%s","Integer value \n");
+    printf("%c has integer value %d\n", 'A', 'A');
+    printf("%c has integer value %d\n", 'B', 'B');
+    printf("%c has integer value %d\n", 'C', 'C');
+    printf("%c has integer value %d\n", 'a', 'a');
+    printf("%c has integer value %d\n", 'b', 'b');
+    printf("%c has integer value %d\n", 'c', 'c');
+    printf("%c has integer value %d\n", '0', '0');
+    printf("%c has integer value %d\n", '1', '1');
+    printf("%c has integer value %d\n", '2', '2');
+    printf("%c has integer value %d\n", '$', '$');
+    printf("%c has integer value %d\n", '*', '*');
+    printf("%c has integer value %d\n", '+', '+');
+    printf("%c has integer value %d\n", '/', '/');
+    printf("%c has integer value %d\n", ' ', ' ');
+}
