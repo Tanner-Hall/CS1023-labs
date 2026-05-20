8a5118f2f7f6da35fef6857388d4587230304e9f
Tanner <Tanner.Hall@unb.ca>
2026-05-20 16:30:33 -0300

    first attempt doesn't work

---
+++ lab3/question3/coinflip.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int flip(void);
+
+int main(void){
+    int neg_count =0;
+    int pos_count =0;
+    int result =0;
+    for (int i =0; i < 100; i++){
+    result = flip();
+    if (result == 1){
+        pos_count++;}
+    else if (result == 0){
+        neg_count++;}
+    }
+printf("coin landed on heads %d times \n", pos_count);
+printf("coin landed on tails %d\n times \n", neg_count);
+}
+
+int flip(void){
+    int ans = 0;
+    srand(time(NULL));
+    ans = (rand() % 3) -1;
+    return ans;
+}
