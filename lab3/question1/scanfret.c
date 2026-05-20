7df06c6a5267f77939df2ac564faaf988d65cc3b
Tanner <Tanner.Hall@unb.ca>
2026-05-20 15:35:31 -0300

    code for the first part of question 1

---
+++ lab3/question1/scanfret.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void){
+    int grade =0;
+    int read =0;
+    puts("enter an integer grade");
+    read = scanf("%d", &grade);
+    printf("%d\n", read);
+    if (read > 0) {
+        printf("The grade you entered was %d\n", grade);
+    }
+}
