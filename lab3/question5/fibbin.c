210dd8a02402d56b20784739a765d011a87378fd
Tanner <Tanner.Hall@unb.ca>
2026-05-23 18:39:17 -0300

    non recursive

--- lab3/question5/fibbin.c
+++ lab3/question5/fibbin.c
@@ -1,20 +1,30 @@
-#include<stdio.h>
+#include <stdio.h>
 
-unsigned long long int fib(int num);
-int main(void){
-int n=0;
-puts("I will calculate the fibinacci sequence up to for the entered value:");
-scanf("%d", &n);
-printf("the corresponding fibinacci number for %d is %llu",n, fib(n));
+unsigned long long int fibonacci(int n);
+
+int main(void) {
+    int n;
+    printf("Enter n: ");
+    scanf("%d", &n);
+    printf("Fibonacci(%d) = %llu\n", n, fibonacci(n));
+    return 0;
 }
-unsigned long long int fib(int num){
-if (num<=1){
-    return num;
-}
-else{
-    return fib(num-1)+fib(num-2);
-}
+
+unsigned long long int fibonacci(int n) {
+    if (n <= 1)
+        return n;
+
+    unsigned long long int prev = 0;
+    unsigned long long int curr = 1;
+
+    for (int i = 2; i <= n; i++) {
+        unsigned long long int next = prev + curr;
+        prev = curr;
+        curr = next;
+    }
+
+    return curr;
 }
 /*Main is used to print the final output and initiate the variable.
-This variable is then passed on to the function fib which is a reocuring funcion that calls on itslef.
-It uss the mathmatical relaion to calculate the fibinacci value for the input.*/
+This variable is then passed on to the function fibonacci which has 2 varables that define the previous and current values.
+fibonacci is comprised of a for loop that adds he current value and previous value for each iteration until the loop reaches the input.*/
