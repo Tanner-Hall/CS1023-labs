3c0756ad76a616c722285baf2a0773708bf138f2
Tanner <Tanner.Hall@unb.ca>
2026-05-27 19:54:01 -0300

    both stages work

--- Lab4/question1/maxmin.c
+++ Lab4/question1/maxmin.c
@@ -2,11 +2,13 @@
 #define SIZE 5
 
 int recursiveMax(const int a[], int n);
+int recursiveMin(const int a[], int n);
 
 int main(void) {
     int array[SIZE] = {4, 7, 9, 6, 2};
 
     printf("the maximum of the array is %d\n", recursiveMax(array, SIZE - 1));
+    printf("the minimum of the array is %d\n", recursiveMin(array, SIZE - 1));
 
     return 0;
 }
@@ -19,4 +21,14 @@ int recursiveMax(const int a[], int n) {
     int maxOfRest = recursiveMax(a, n - 1);
 
     return (a[n] > maxOfRest) ? a[n] : maxOfRest;
+}
+
+int recursiveMin(const int a[], int n) {
+    if (n == 0) {
+        return a[0];
+    }
+
+    int minOfRest = recursiveMin(a, n - 1);
+    
+    return (a[n] < minOfRest) ? a[n] : minOfRest;
 }
