2244725cb286fc18a130ba505c7b712a854b1935
Tanner <Tanner.Hall@unb.ca>
2026-05-27 19:53:37 -0300

    got max to work

--- Lab4/question1/maxmin.c
+++ Lab4/question1/maxmin.c
@@ -1,42 +1,22 @@
 #include <stdio.h>
-#define size 5
-int recursiveMax(const int a[],int max, int n);
-int recursiveMin(const int a[],int max, int n);
+#define SIZE 5
 
-int main(void){
-int start=0;
-int ma = size;
-int mi = size;
-int array[size]= {4,7,9,6,2};
-printf("the maximum of the array is %d\n", recursiveMax(array, start, ma));
-printf("the minimum of the array is %d\n", recursiveMin(array, start, mi));
+int recursiveMax(const int a[], int n);
 
+int main(void) {
+    int array[SIZE] = {4, 7, 9, 6, 2};
+
+    printf("the maximum of the array is %d\n", recursiveMax(array, SIZE - 1));
+
+    return 0;
 }
-int recursiveMax(const int a[], int max, int n){
-    if (a[n]>a[n+1]){
-        max = a[n];
-    }
-    if (a[n]<a[n+1]){
-        max = a[n+1];
-    }
-    if (n == 1){
-        if (max < a[size]){
-             max = a[size];
-        return max;}
-    }
-    return (recursiveMax(a, max, n+1));
+
+int recursiveMax(const int a[], int n) {
+    if (n == 0) {
+        return a[0];
     }
 
-int recursiveMin(const int a[], int min, int n){
-    if (a[n]>a[n+1]){
-        min = a[n+1];}
-    if (a[n]<a[n+1]){
-        min = a[n];
-    }
-    if (n == size -1){
-        if (min > a[size]){
-             min = a[size];
-        return min;}
-    }
-    return (recursiveMin(a, min, n+1));
-    }
+    int maxOfRest = recursiveMax(a, n - 1);
+
+    return (a[n] > maxOfRest) ? a[n] : maxOfRest;
+}
