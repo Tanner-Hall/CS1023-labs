4f2122ca944c2b658ecdd036ac8a3cb739faf8ad
Tanner <Tanner.Hall@unb.ca>
2026-05-27 16:04:39 -0300

    changed the array to not be constant and added for loop

--- Lab4/question3/sortcol.c
+++ Lab4/question3/sortcol.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #define size 7
-void sort(const int a[], int n);
+void sort(int a[], int n);
 int main(void){
 int b = size; 
 int array[size]= {2,0,1,1,2,0,0};
@@ -10,18 +10,19 @@ for (size_t i =0; i < size; i++ ){
 }
 sort(array, b);
 }
-void sort(const int a[], int n){
+void sort(int a[], int n){
 if (n -1 == 0){
     printf("sorted array is ");
     for (size_t i =0; i < size; i++ ){
     printf(" %d", a[i]);
 }
 }
-    if (a[n] < a[n-1]){
-        int temp = a[n-1];
-        a[n-1] = a[n];
-        a[n] = temp;
+    for (size_t i =1; i <= n; i++ )
+    if (a[i] < a[i-1]){
+        int temp = a[i-1];
+        a[i-1] = a[i];
+        a[i]= temp;
     }
-
+sort(a, n-1);
 
 }
