ca14ae7eb5ed0ba37e92ba83fd69169d1591cc30
Tanner <Tanner.Hall@unb.ca>
2026-06-10 15:46:38 -0300

    final working code

--- Lab5/question3/rightshift.c
+++ Lab5/question3/rightshift.c
@@ -6,17 +6,16 @@ puts("enter a positive integer");
 int x =0;
 scanf("%d", &x);
 printf("your number is %d\n", x);
-mask <<= 31;
-for(int c =1; c<=32; c++){
-putchar((x & mask) ? '1' : '0');
+for(int c =31; c >=0; c--){
+putchar((x & (mask << c)) ? '1' : '0');
 if (c % 8 == 0){
 putchar(' ');}
 }
 puts("\n");
 int y = x >>= 4;
 printf("the shifted result is %d\n", y);
-for(int c =1; c<=32; c++){
-putchar((y & mask) ? '1' : '0');
+for(int c =31; c >=0; c--){
+putchar((y & (mask << c)) ? '1' : '0');
 if (c % 8 == 0){
 putchar(' ');}
 }
