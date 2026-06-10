4492fddd5e1d5cd88c1aadd72fd71c60777847d0
Tanner <Tanner.Hall@unb.ca>
2026-06-10 15:43:38 -0300

    gives wrog value for binary representation

--- Lab5/question3/rightshift.c
+++ Lab5/question3/rightshift.c
@@ -8,14 +8,15 @@ scanf("%d", &x);
 printf("your number is %d\n", x);
 mask <<= 31;
 for(int c =1; c<=32; c++){
-putchar((x & mask) ? "1" : "0");
+putchar((x & mask) ? '1' : '0');
 if (c % 8 == 0){
 putchar(' ');}
 }
+puts("\n");
 int y = x >>= 4;
 printf("the shifted result is %d\n", y);
 for(int c =1; c<=32; c++){
-putchar((y & mask) ? "1" : "0");
+putchar((y & mask) ? '1' : '0');
 if (c % 8 == 0){
 putchar(' ');}
 }
