11f6da9830f4368b17322d20c28eb53af67d6ec9
Tanner <Tanner.Hall@unb.ca>
2026-06-11 08:32:09 -0300

    function added to create packed unsigned int

--- Lab5/question4/pack.c
+++ Lab5/question4/pack.c
@@ -7,9 +7,9 @@ char a[4]= {};
 unsigned int x =0;
 int mask =1;
 printf("an unsigned integer is %zu bits\n", CHAR_BIT * sizeof(x));
-for (int i =0; i <= 4; i++){
+for (int i =0; i < 4; i++){
 printf("enter character number %d\n", i);
-scanf("%s", &a[i]);
+scanf("%c", &a[i]);
 printf("character %d is %d\n", i, a[i]);
 printf("the 8 bit representation of the character is:");
 for(int c =7; c >=0; c--){
@@ -17,8 +17,12 @@ putchar((a[i] & (mask << c)) ? '1' : '0');
 }
 }
 }
-void packing(const char array[]){
+unsigned int packCharacters(const char array[]) {
+    unsigned int packed = 0;
 
+    for (int i = 0; i < 4; i++) {
+        packed = (packed << 8) | (unsigned char)array[i];
+    }
 
-
+    return packed;
 }
