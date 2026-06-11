b9f97529047e6bb9c7ebc4f734eee3c264f45730
Tanner <Tanner.Hall@unb.ca>
2026-06-11 08:54:10 -0300

    sent entered inputs to union and printed when stored as float

--- Lab5/question4/union.c
+++ Lab5/question4/union.c
@@ -25,3 +25,12 @@ int main(void) {
     scanf("%Lf", &temp_x);
     printf("\n--------------------------------------------------\n\n");
 
+    u1.f = temp_f;
+    u2.d = temp_d;
+    u3.x = temp_x;
+
+    printf("--- Union 1 (Stored as float '%f') ---\n", temp_f);
+    printf("As float:       %f\n", u1.f);
+    printf("As double:      %f\n", u1.d);
+    printf("As long double: %Lf\n\n", u1.x);
+
